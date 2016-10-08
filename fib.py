535954791a86f2f20c69a4fe639ecf7b8d9b7385
Chase Qi <qiziming1@gmail.com>
2016-10-08 05:20:59 -0400

    added the part where it print it right away

--- fib.py
+++ fib.py
@@ -1,7 +1,22 @@
 counter=input("which one you want to print")
 a=0
 b=1
+c=0
+d=1
 n=0
+m=0
+while m<counter:
+	if(m%2==0):
+		if(m==counter-1):
+			print c
+		c=c+d
+		m=m+1
+	if(m%2==1):
+		if(m==counter-1):
+			print d
+		d=c+d
+		m=m+1
+		
 while n<counter:
 
 	print a
@@ -12,5 +27,5 @@ while n<counter:
 		b=a+b
 		n=n+1
 
-	
+
 
