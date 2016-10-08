2092af5a9a8b46242af543edaf87452728ba8d02
Chase Qi <qiziming1@gmail.com>
2016-10-08 05:41:12 -0400

    unnecessary assignment

--- fibTable.py
+++ fibTable.py
@@ -1,12 +1,10 @@
 counter=input("how many lines you want to print")
-lineCounter=0
 flist=list()
 flist=[1]
 for x in range(1,counter):
 	print flist
 	newList=list(flist)
 	newList.append(1)
-	newList[0]=1
 	for y in range(1,x):
 		newList[y]=flist[y]+flist[y-1]
 	flist=newList
