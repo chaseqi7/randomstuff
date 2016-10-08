73e6bb734d32992ffc9c6b9a546044010146bbb4
Chase Qi <qiziming1@gmail.com>
2016-10-08 04:40:20 -0400

    changed 1 to “\” and “/”

---
+++ fib.py
@@ -0,0 +1,4 @@
+n=input("how many lines you want to print")
+lineNumber=1
+for x in range(0, n):
+	standardList.append("o")
---
+++ real-x-generator.py
@@ -0,0 +1,14 @@
+n=input("pls input rows and cols")
+standardList=list()
+for x in range(0, n):
+	standardList.append("o")
+first=0
+last=n-1
+for x in range(0, n):
+	new_list = list(standardList)
+	new_list[first]='\\'
+	new_list[last]="/"
+	print new_list
+	first=first+1
+	last=last-1
+print '\\'
